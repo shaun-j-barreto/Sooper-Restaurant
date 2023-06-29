6f2a4cc53a6c462faac00ebd1e46af416235b766
OBLIABLO <shaunjesusbarreto@gmail.com>
2023-06-29 22:23:38 +0530

    hamburger bug-fixes, added about and menu componenets

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 window.onload = function () {
-
     const menu_button = document.querySelector('.hamburger');
     const hamburger_menu = document.querySelector('.ham-navbar');
-    
-    menu_button.addEventListener('click',function () {
+
+    menu_button.addEventListener('click', function () {
         hamburger_menu.classList.toggle('is-active');
         menu_button.classList.toggle('is-active');
-        
+    });
+
+    const menu_links = hamburger_menu.querySelectorAll('#close-hamburger');
+
+    menu_links.forEach(function (link) {
+        link.addEventListener('click', function () {
+            hamburger_menu.classList.remove('is-active');
+            menu_button.classList.remove('is-active');
+        });
     });
 };
